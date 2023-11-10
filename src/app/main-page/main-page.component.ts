a4c297d6ce4bd939ccf4d1c8f425ffbac4e866e0
Shir0Shi <not.just.white@gmail.com>
2023-11-10 18:35:13 +0100

    edited photo path

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -15,6 +15,6 @@ export class MainPageComponent {
   yearOfStudy: number = 2023;
   professor: string = 'Tetiana Kovalur';
   labProfessor: string = 'Anastasiia Nikolaienko';
-  myPhoto = "assets/myPhoto.jpeg";
+  myPhoto = 'assets/myPhoto.jpeg';
 
 }
